d363823ca7c948ec75da446c2b9cbd3ae13ef6ae
Nicholas Åhman <nicholas.ahman@gmail.com>
2023-09-18 12:41:42 +0300

    Initial commit

---
+++ lua/config/init.lua
@@ -0,0 +1,5 @@
+require("config.remap")
+require("config.packer")
+
+vim.o.background = "dark" 
+vim.cmd([[colorscheme gruvbox]])
---
+++ lua/config/packer.lua
@@ -0,0 +1,30 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use { "ellisonleao/gruvbox.nvim" }
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+
+end)	
