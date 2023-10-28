2f70cd7e77672742924689042e1fb5c3bca44f8c
Nicholas Åhman <nicholas.ahman@gmail.com>
2023-10-28 15:48:34 +0300

    Grep command

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,11 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+function MyVimgrep(input)
+  local search_pattern = vim.fn.input('Enter search pattern: ', input)
+  vim.cmd('vimgrep /' .. search_pattern .. '/j **/*')
+  vim.cmd('copen')
+end
+
+vim.api.nvim_set_keymap('n', '<Leader>gr', [[:lua MyVimgrep('')<CR>]], { noremap = true, silent = true })
